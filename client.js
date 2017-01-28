c8d2b1cb0ef1a894f67e88be867de75a0fdf643d
Rob Murrer <robmurrer@gmail.com>
2017-01-28 13:41:56 -0800

    start/stop adding time now implemented.

--- client.js
+++ client.js
@@ -8,28 +8,34 @@ var Events = (function() {
   } 
 
   var cur_state = STATE.RESET;
+  var prev_delta = 0.0; 
 
   var counter = 0;
   var start_time;
+
+  var start_tick = function() {
+    start_time = Date.now();
+    counter = window.setInterval(tick, 10);
+    Elements.start_stop_button.innerText = "Stop";
+    cur_state = STATE.PLAYING;
+  };
 
   var start_stop = function() { 
     switch (cur_state)
     {
       case STATE.RESET:
-        start_time = Date.now();
-        counter = window.setInterval(tick, 10);
-        Elements.start_stop_button.innerText = "Stop";
-        cur_state = STATE.PLAYING;
+        start_tick();
       break;
 
       case STATE.PLAYING:
+        prev_delta = Date.now() - start_time + prev_delta;
         window.clearInterval(counter);
         Elements.start_stop_button.innerText = "Start";
         cur_state = STATE.PAUSED;
       break;
 
       case STATE.PAUSED:
-        console.log("State: Paused");
+        start_tick();
       break;
       
       default:
@@ -41,7 +47,7 @@ var Events = (function() {
   };
 
   var tick = function() {
-    var delta = Date.now() - start_time; 
+    var delta = Date.now() - start_time + prev_delta;
     var ms = parseInt((delta % 1e3))
     var seconds = parseInt(delta / 1e3);
     var minutes = parseInt(seconds / 60);
@@ -52,8 +58,8 @@ var Events = (function() {
 
   var reset = function () {
     Elements.display.innerText = "00:00:00";
+    prev_delta = 0.0;
     cur_state = STATE.RESET;
-
   };
 
 

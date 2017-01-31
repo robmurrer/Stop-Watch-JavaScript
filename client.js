d6216385f5ab2ed49b8a941baa87359f3b043786
Rob Murrer <robmurrer@gmail.com>
2017-01-31 15:25:44 -0800

    Refactor to Fat Model View.

--- client.js
+++ client.js
@@ -1,95 +1,122 @@
 "use strict";
 
-var Events = (function() {
+var Stopwatch = (function() {
+
   var STATE = {
-    RESET: 0,
-    PLAYING: 1,
-    PAUSED: 2
+    STOPPED: 0,
+    RUNNING: 1,
   } 
 
-  var cur_state = STATE.RESET;
-  var prev_delta = 0.0; 
+  var Model = (function () {
+    var state = STATE.STOPPED;
+    var delta = 0.0; 
+    var prev_delta = 0.0; 
+    var counter = 0;
+    var start_time;
 
-  var counter = 0;
-  var start_time;
+    // private
+    var CalcDelta = function() { 
+      return Date.now() - start_time + prev_delta;
+    };
 
-  var start_tick = function() {
-    start_time = Date.now();
-    counter = window.setInterval(tick, 10);
-    Elements.start_stop_button.innerText = "Stop";
-    cur_state = STATE.PLAYING;
-  };
+    var tick = function() {
+      delta = CalcDelta(); 
+      View.display.innerText = parse_delta(delta);
+    };
 
-  var pause = function(prev_delta) {
-      window.clearInterval(counter);
-      Elements.start_stop_button.innerText = "Start";
-      cur_state = STATE.PAUSED;
-  };
+    var parse_delta = function(delta) {
+      var ms = parseInt((delta % 1e3))
+      var seconds = parseInt(delta / 1e3);
+      var minutes = parseInt(seconds / 60);
+      var hours = parseInt(minutes / 60);
 
-  var start_stop = function() { 
-    switch (cur_state)
+      return minutes%60 + ":" + seconds%60 + ":" + ms%60;
+    };
+
+
+    //public api
+    var GetState = function() { return state; };
+    var Start = function() {
+      start_time = Date.now();
+      counter = window.setInterval(tick, 10);
+      View.start_stop_button.innerText = "Stop";
+      state = STATE.RUNNING;
+    };
+
+    var Stop = function() {
+        window.clearInterval(counter);
+        View.start_stop_button.innerText = "Start";
+        state = STATE.STOPPED;
+    };
+
+    var Reset = function () {
+      View.display.innerText = parse_delta(0);
+      state = STATE.STOPPED;
+    };
+
+    return {
+      Start: Start, 
+      Stop: Stop,
+      Reset: Reset,
+      GetState: GetState,
+    }
+
+  })();
+
+
+
+  var Start_Stop_Click = function() { 
+    switch (Model.GetState())
     {
-      case STATE.RESET:
-        start_tick();
+      case STATE.STOPPED:
+        Model.Start();
       break;
 
-      case STATE.PLAYING:
-        pause(Date.now() - start_time + prev_delta);
-      break;
-
-      case STATE.PAUSED:
-        start_tick();
+      case STATE.RUNNING:
+        Model.Stop();
       break;
       
       default:
       break;
 
     }
-
-
   };
 
-  var parse_delta = function(delta) {
-    var ms = parseInt((delta % 1e3))
-    var seconds = parseInt(delta / 1e3);
-    var minutes = parseInt(seconds / 60);
-    var hours = parseInt(minutes / 60);
-
-    return minutes%60 + ":" + seconds%60 + ":" + ms%60;
-  }
-
-  var tick = function() {
-    var delta = Date.now() - start_time + prev_delta;
-    Elements.display.innerText = parse_delta(delta);
-  };
-
-  var reset = function () {
-    Elements.display.innerText = parse_delta(0);
-    pause(0);
-    cur_state = STATE.RESET;
+  var Reset_Click = function() 
+  {
+    Model.Reset();
   };
 
 
-  return {
-    start_stop: start_stop,
-    reset: reset,
-  };
+  var View = (function() {
+    var reset_text = "Reset";
+    var start_text = "Start";
+    var stop_text = "Stop";
 
- })();
+    var stopwatch = document.getElementById("stopwatch");
+
+    var display = document.createElement("div");
+    display.id = "display";
+    stopwatch.appendChild(display)
+
+    var start_stop_button = document.createElement("button");
+    start_stop_button.id = "start_stop";
+    start_stop_button.innerText = start_text;
+    start_stop_button.addEventListener("click", Start_Stop_Click);
+    stopwatch.appendChild(start_stop_button);
+
+    var reset_button = document.createElement("button");
+    reset_button.id = "reset";
+    reset_button.innerText = reset_text; 
+    reset_button.addEventListener("click", Reset_Click); 
+    stopwatch.appendChild(reset_button);
+
+    return {
+      start_stop_button: start_stop_button,
+      reset_button: reset_button,
+      display: display
+    };
+  })();
 
 
-var Elements = (function() {
-  var start_stop_button = document.getElementById("start_stop_button");
-  start_stop_button.addEventListener("click", Events.start_stop);
-
-  var reset_button = document.getElementById("reset_button");
-  reset_button.addEventListener("click", Events.reset);
-
-  var display = document.getElementById("display");
-  return {
-    start_stop_button: start_stop_button,
-    reset_button: reset_button,
-    display: display
-  };
 })();
-
